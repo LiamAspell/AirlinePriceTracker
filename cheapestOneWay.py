58928e92dab61920ecf42fa08015b057b9d2409f
LiamAspell <liamaspell123@gmail.com>
2024-05-23 14:46:16 +0100

    Added writing to CSV File

--- cheapestOneWay.py
+++ cheapestOneWay.py
@@ -1,25 +1,41 @@
+import csv
 import requests
 import json
 from config import API_KEY
 from searchCriteria import DEPARTURE_AIRPORT,ARRIVAL_AIRPORT,DEPARTURE_DATE,CURRENCY,CABIN_CLASS,ADULTS 
 
-url = "https://sky-scanner3.p.rapidapi.com/flights/cheapest-one-way"
+def writeToJSON(data): 
+    with open('results/cheapestOneWay.json', 'w') as f:
+        json.dump(data, f, indent=4)
+        print("Prices have been saved to prices.json file.")
 
-querystring = {"fromEntityId":DEPARTURE_AIRPORT,"toEntityId":ARRIVAL_AIRPORT,"departDate":DEPARTURE_DATE, "currency":CURRENCY, "cabinClass":CABIN_CLASS, "adults":ADULTS}
+def writeToCSV(data): 
+    with open('results/cheapestOneWay.csv', 'w', newline='') as f:
+        writer = csv.DictWriter(f, fieldnames=['day', 'group', 'price'])
+        writer.writeheader()
+        for item in data["data"]:
+            writer.writerow(item)
+        print("Prices have been saved to cheapestOneWay.csv file.")
 
-headers = {
-	"X-RapidAPI-Key": API_KEY,
-	"X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
-}
+def makeAPICall(): 
+    url = "https://sky-scanner3.p.rapidapi.com/flights/cheapest-one-way"
 
-response = requests.get(url, headers=headers, params=querystring)
+    querystring = {"fromEntityId":DEPARTURE_AIRPORT,"toEntityId":ARRIVAL_AIRPORT,"departDate":DEPARTURE_DATE, "currency":CURRENCY, "cabinClass":CABIN_CLASS, "adults":ADULTS}
+
+    headers = {
+	    "X-RapidAPI-Key": API_KEY,
+	    "X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
+    }
+
+    response = requests.get(url, headers=headers, params=querystring)
+
+    data = response.json()
+
+    # Write the data to a JSON file
+    writeToJSON(data)
+    # Write the data to a CSV file
+    writeToCSV(data)
+    
+makeAPICall()
 
 
-
-data = response.json()
-
-
-# Write the data to a JSON file
-with open('cheapestOneWay.json', 'w') as f:
-    json.dump(data, f, indent=4)
-    print("Prices have been saved to prices.json file.")
