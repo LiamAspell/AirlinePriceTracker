6c8bb78377434b7d37fed986e1da09484cc0627d
LiamAspell <liamaspell123@gmail.com>
2024-06-14 17:55:16 +0100

    Multiple Airports can be req'd at once

--- cheapestOneWay.py
+++ cheapestOneWay.py
@@ -9,8 +9,8 @@ def writeToJSON(data):
         json.dump(data, f, indent=4)
         print("Prices have been saved to prices.json file.")
 
-def writeToCSV(data): 
-    with open('results/cheapestOneWay.csv', 'w', newline='') as f:
+def writeToCSV(airport, data): 
+    with open(f'results/{airport}.csv', 'w', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=['day', 'group', 'price'])
         writer.writeheader()
         for item in data["data"]:
@@ -18,23 +18,27 @@ def writeToCSV(data):
         print("Prices have been saved to cheapestOneWay.csv file.")
 
 def makeAPICall(): 
-    url = "https://sky-scanner3.p.rapidapi.com/flights/cheapest-one-way"
+    
+    Arrival_Airports = ["AMS", "BCN", "CDG", "FCO", "FRA", "LHR", "SPU"]
+    
+    for airport in Arrival_Airports:
+        url = "https://sky-scanner3.p.rapidapi.com/flights/cheapest-one-way"
 
-    querystring = {"fromEntityId":DEPARTURE_AIRPORT,"toEntityId":ARRIVAL_AIRPORT,"departDate":DEPARTURE_DATE, "currency":CURRENCY, "cabinClass":CABIN_CLASS, "adults":ADULTS}
+        querystring = {"fromEntityId":DEPARTURE_AIRPORT,"toEntityId":airport,"departDate":DEPARTURE_DATE, "currency":CURRENCY, "cabinClass":CABIN_CLASS, "adults":ADULTS}
 
-    headers = {
-	    "X-RapidAPI-Key": API_KEY,
-	    "X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
-    }
+        headers = {
+	        "X-RapidAPI-Key": API_KEY,
+	        "X-RapidAPI-Host": "sky-scanner3.p.rapidapi.com"
+        }
 
-    response = requests.get(url, headers=headers, params=querystring)
+        response = requests.get(url, headers=headers, params=querystring)
 
-    data = response.json()
+        data = response.json()
 
-    # Write the data to a JSON file
-    writeToJSON(data)
-    # Write the data to a CSV file
-    writeToCSV(data)
+        # Write the data to a JSON file
+        # writeToJSON(airport,data)
+        # Write the data to a CSV file
+        writeToCSV(airport, data)
     
 makeAPICall()
 
