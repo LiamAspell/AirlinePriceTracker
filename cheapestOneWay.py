019c3bc62f633d8067052941bf7ecf2791948fbe
LiamAspell <liamaspell123@gmail.com>
2024-10-26 17:25:01 +0100

    Added docstrings and changed method names to snake case

--- cheapestOneWay.py
+++ cheapestOneWay.py
@@ -11,20 +11,34 @@ if not API_KEY:
     except ImportError:
         raise RuntimeError("API_KEY not found in environment variables or config.py")
 
-def writeToJSON(data): 
+def write_to_json(data): 
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+    """
     with open('departures/cheapestOneWay.json', 'w') as f:
         json.dump(data, f, indent=4)
         print("Prices have been saved to prices.json file.")
 
-def writeToCSV(airport, data): 
+def write_to_csv(airport, data):
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+    """
     with open(f'departures/departure.csv', 'w', newline='') as f:
         writer = csv.DictWriter(f, fieldnames=['day', 'group', 'price'])
         writer.writeheader()
         for item in data["data"]:
             writer.writerow(item)
 
-def makeAPICall(): 
-    
+def make_api_request(): 
+    """_summary_
+
+    Args:
+        data (_type_): _description_
+    """
     Arrival_Airports = ["AMS"] 
     
     for airport in Arrival_Airports:
@@ -39,10 +53,10 @@ def makeAPICall():
         response = requests.get(url, headers=headers, params=querystring)
         data = response.json()
         
-        writeToCSV(airport, data)
+        write_to_csv(airport, data)
     
     except requests.RequestException as e:
         print(f"Error fetching data from API: {e}")
-makeAPICall()
+make_api_request()
 
 
